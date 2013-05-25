cd9aca69189212e02f419f2b4a767bb3aa642a25
Asko Soukka <asko.soukka@iki.fi>
2013-05-25 08:10:37 +0300

    Fix version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="robotframework-wavelibrary",
-    version="1.0.0",
+    version="0.1.0",
     description="Robot Framework resources for automating WebAIM Wave Toolbar",
     long_description=(open("README.rst").read() + "\n" +
                       open("CHANGES.txt").read()),
