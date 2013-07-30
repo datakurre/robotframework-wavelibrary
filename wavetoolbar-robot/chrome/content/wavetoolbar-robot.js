b20cfc397e75dc2c3ade23eb5fc6959711a62cb4
Asko Soukka <asko.soukka@iki.fi>
2013-07-30 09:56:00 +0300

    Re-indent

--- wavetoolbar-robot/chrome/content/wavetoolbar-robot.js
+++ wavetoolbar-robot/chrome/content/wavetoolbar-robot.js
@@ -1,18 +1,18 @@
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
-var myObserver = {
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver,
-                                         Ci.nsISupportsWeakReference]),
-  observe: function(subject, topic, data) {
-    if (topic == "content-document-global-created" &&
-        subject instanceof Ci.nsIDOMWindow) {
-      XPCNativeWrapper.unwrap(subject).wave_viewIcons=wave_viewIcons;
-      XPCNativeWrapper.unwrap(subject).wave_viewReset=wave_viewReset;
+var WAVEToolbarRobotObserver = {
+    QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver,
+        Ci.nsISupportsWeakReference]),
+    observe: function(subject, topic, data) {
+        if (topic == "content-document-global-created" &&
+            subject instanceof Ci.nsIDOMWindow) {
+            XPCNativeWrapper.unwrap(subject).wave_viewIcons=wave_viewIcons;
+            XPCNativeWrapper.unwrap(subject).wave_viewReset=wave_viewReset;
+        }
     }
-  }
 };
 
 var observerService = Cc["@mozilla.org/observer-service;1"]
-                        .getService(Ci.nsIObserverService);
-observerService.addObserver(myObserver,
-                            "content-document-global-created", true);
+    .getService(Ci.nsIObserverService);
+observerService.addObserver(WAVEToolbarRobotObserver,
+    "content-document-global-created", true);
